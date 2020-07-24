0d756f40bb638e9bf47748c7504c95e28b2e70d3
d_yama <d_yama@hololab.co.jp>
2020-07-24 20:50:53 +0900

    [add]threeを導入した

--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,7 @@
       <b-card-title>
         {{welcomeMessage}}
       </b-card-title>
+      <paint/>
     </b-card>
   </div>
 </template>
@@ -11,8 +12,10 @@
 <script lang="ts">
   import Vue from 'vue'
   import { Component, Prop, Watch } from 'nuxt-property-decorator'
-
-  @Component
+  import Paint from '../components/ar/paint.vue'
+  @Component({
+    components: {Paint}
+  })
   export default class extends Vue {
     public get welcomeMessage() {
       return 'welcome to webxr playground.'

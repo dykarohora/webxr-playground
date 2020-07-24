a52a97fc38cabe730c82b93cb1437257fa541922
d_yama <d_yama@hololab.co.jp>
2020-07-24 23:29:47 +0900

    hittest

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,5 @@
 <template>
-  <div class="home">
-  </div>
+  <div></div>
 </template>
 
 <script lang="ts">
@@ -8,7 +7,6 @@
   import * as THREE from 'three'
 
   import { ARButton } from '../components/ar/ARButton'
-  import { TubePainter } from '../components/ar/TubePainter'
 
   import { Component } from 'nuxt-property-decorator'
 
@@ -18,10 +16,11 @@
     private scene!: THREE.Scene
     private camera!: THREE.Camera
     private renderer!: THREE.WebGLRenderer
-    private painter!: TubePainter
     private controller!: THREE.Group
+    private reticle!: THREE.Mesh
 
-    private cursor = new THREE.Vector3()
+    private hitTestSource: any = null
+    private hitTestSourceRequested: boolean = false
 
     mounted() {
       this.init()
@@ -35,39 +34,41 @@
       this.scene = new THREE.Scene()
       this.camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 20)
 
+      const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1)
+      light.position.set(0, 1, 0)
+      this.scene.add(light)
+
       this.renderer = new THREE.WebGLRenderer({antialias: true, alpha: true})
       this.renderer.setPixelRatio(window.devicePixelRatio)
       this.renderer.setSize(window.innerWidth, window.innerHeight)
       this.renderer.xr.enabled = true
       container.appendChild(this.renderer.domElement)
 
-      document.body.appendChild(ARButton.createButton(this.renderer))
+      document.body.appendChild(ARButton.createButton(this.renderer, {requiredFeatures: ['hit-test']}))
 
-      const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1)
-      light.position.set(0, 1, 0)
-      this.scene.add(light)
+      const geometry = new THREE.CylinderBufferGeometry(0.1, 0.1, 0.2, 32).translate(0, 0.1, 0)
 
-      this.painter = new TubePainter()
-      this.painter.setSize(0.4)
-      //@ts-ignore
-      this.painter.mesh.material.side = THREE.DoubleSide
-      this.scene.add(this.painter.mesh)
 
       this.controller = this.renderer.xr.getController(0)
-      this.controller.addEventListener(
-        'selectstart', () => {
-          this.controller.userData.isSelecting = true
-          this.controller.userData.skipFrames = 2
+      this.controller.addEventListener('select', () => {
+        if (this.reticle.visible) {
+          const material = new THREE.MeshPhongMaterial({color: 0xffffff * Math.random()})
+          const mesh = new THREE.Mesh(geometry, material)
+          mesh.position.setFromMatrixPosition(this.reticle.matrix)
+          mesh.scale.y = Math.random() * 2 + 1
+          this.scene.add(mesh)
         }
-      )
-      this.controller.addEventListener(
-        'selectend', () => {
-          this.controller.userData.isSelecting = false
-        }
-      )
+      })
 
-      this.controller.userData.skipFrames = 0
       this.scene.add(this.controller)
+
+      this.reticle = new THREE.Mesh(
+        new THREE.RingBufferGeometry(0.15, 0.2, 32).rotateX(-Math.PI / 2),
+        new THREE.MeshBasicMaterial()
+      )
+      this.reticle.matrixAutoUpdate = false
+      this.reticle.visible = false
+      this.scene.add(this.reticle)
 
       window.addEventListener('resize', () => {
         //@ts-ignore
@@ -78,28 +79,63 @@
       }, false)
     }
 
-    handleController(controller: THREE.Group) {
-      const userData = controller.userData
-
-      this.cursor.set(0, 0, -0.2).applyMatrix4(controller.matrixWorld)
-      if (userData.isSelecting) {
-        if (userData.skipFrames >= 0) {
-          userData.skipFrames--
-          this.painter.moveTo(this.cursor)
-        } else {
-          this.painter.lineTo(this.cursor)
-          this.painter.update()
-        }
-      }
-    }
-
-    render() {
-      this.handleController(this.controller)
-      this.renderer.render(this.scene, this.camera)
-    }
-
     animate() {
       this.renderer.setAnimationLoop(this.render)
+    }
+
+    render(timestamp: any, frame: any) {
+
+      if (frame) {
+
+        const referenceSpace = this.renderer.xr.getReferenceSpace()
+        const session = this.renderer.xr.getSession()
+
+        if (this.hitTestSourceRequested === false) {
+
+          session.requestReferenceSpace('viewer').then((referenceSpace: any) => {
+
+            session.requestHitTestSource({space: referenceSpace}).then((source: any) => {
+
+              this.hitTestSource = source
+
+            })
+
+          })
+
+          session.addEventListener('end', () => {
+
+            this.hitTestSourceRequested = false
+            this.hitTestSource = null
+
+          })
+
+          this.hitTestSourceRequested = true
+
+        }
+
+        if (this.hitTestSource) {
+
+          const hitTestResults = frame.getHitTestResults(this.hitTestSource)
+
+          if (hitTestResults.length) {
+
+            const hit = hitTestResults[0]
+
+            this.reticle.visible = true
+            this.reticle.matrix.fromArray(hit.getPose(referenceSpace).transform.matrix)
+
+          } else {
+
+            this.reticle.visible = false
+
+          }
+
+        }
+
+      }
+
+      this.renderer.render(this.scene, this.camera)
+
     }
   }
 </script>
